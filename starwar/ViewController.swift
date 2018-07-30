fbf37fbcf9e34ffab33552245d9a0aa695cee49f
Jack Huang <jackhuang@jackde-macbook-air.local>
2018-07-30 18:41:25 +0800

    Initial Commit

---
+++ starwar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  starwar
+//
+//  Created by Jack Huang on 2018/7/30.
+//  Copyright © 2018 Jack Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
